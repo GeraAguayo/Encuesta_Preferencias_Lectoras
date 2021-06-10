66effa976c8747bf04b6f317ebfa414fffa5405b
GerardoAguayo <geraaguayoc@gmail.com>
2021-06-10 11:07:26 -0500

    Update Encuesta.java
    Corrección de errores en la ortografía

--- Encuesta.java
+++ Encuesta.java
@@ -6,14 +6,14 @@ public class Encuesta {
         char opcion;
 
         //Se inicia preguntandole al usuario que tipo de textos prefiere
-        JOptionPane.showMessageDialog(null, "Hola! , en este cuestionario se le recomendara una serie de libros basado en sus gustos.\n Por favor, en cada pregunta solo elija una opcion.");
-        opcion = JOptionPane.showInputDialog("Generalmente, ¿que tipo de textos disfruta mas? \nEscriba R para indicar que realistas o escriba F para indicar que ficticios.").charAt(0);
+        JOptionPane.showMessageDialog(null, "¡Hola! En este cuestionario se le recomendará una serie de libros basados en sus gustos.\n Por favor, en cada pregunta solo elija una opción.");
+        opcion = JOptionPane.showInputDialog("Generalmente, ¿Qué tipo de textos disfruta más? \nEscriba R para indicar que realistas o escriba F para indicar que ficticios.").charAt(0);
         if (opcion == 'R' || opcion == 'r')//Lo que pasa si elige un texto realista
         {
-            opcion = JOptionPane.showInputDialog("¿Por que tipo de texto se siente mas atraido? Algo que trate acerca de historia(escriba H), \nUna novela(escriba N) o un texto cientifico(Escriba C)").charAt(0);
+            opcion = JOptionPane.showInputDialog("¿Por qué tipo de texto se siente más atraído? Algo que trate acerca de historia(escriba H), \nUna novela(escriba N) o un texto científico(Escriba C)").charAt(0);
             if (opcion == 'H' ||  opcion == 'h')//Lo que  pasa si elige historico
             {
-                opcion = JOptionPane.showInputDialog("¿Prefiere la biografia de una persona(Escriba P) o la historia de algun lugar en general (Escriba G)\n o alguna guerra famosa (Escriba W)").charAt(0);
+                opcion = JOptionPane.showInputDialog("¿Prefiere la biografía de una persona(Escriba P) o la historia de algún lugar en general (Escriba G)\n o alguna guerra famosa (Escriba W)").charAt(0);
                 if (opcion == 'P' || opcion == 'p')//Lo que pasa si elige biografia
                 {
                     //RECOMENDACION DE LAS BIOGRAFIAS
@@ -32,7 +32,7 @@ public class Encuesta {
             }//Terminan los libros hsitoricos
             else if (opcion == 'N' || opcion == 'n')//Lo que pasa si elige novela sencilla
             {
-                opcion = JOptionPane.showInputDialog("Prefiere las novelas juveniles (Escriba J)\n o prefiere las novelas romanticas(Escriba R)").charAt(0);
+                opcion = JOptionPane.showInputDialog("Prefiere las novelas juveniles (Escriba J)\n o prefiere las novelas románticas(Escriba R)").charAt(0);
                 if (opcion == 'J' || opcion == 'j')//Lo que pasa si elige una novela juvenil
                 {
                     //RECOMENDACION NOVELA JUVENIL
@@ -46,7 +46,7 @@ public class Encuesta {
             }//Terminan las novelas
             else if (opcion == 'C' || opcion == 'c' )//Lo que pasa si elige textos cientificos
             {
-                opcion = JOptionPane.showInputDialog("A la hora de leer un texto cientifico, lo prefiere relacionado a la biologia(Escriba B),\n relacionado a la física (Escriba F) o relacionado a la Astronomia(Escriba A)").charAt(0);
+                opcion = JOptionPane.showInputDialog("A la hora de leer un texto científico, lo prefiere relacionado a la biología(Escriba B),\n relacionado a la Física (Escriba F) o relacionado a la Astronomía(Escriba A)").charAt(0);
                 if (opcion == 'B' || opcion == 'b')//Lo que pasa si eligio Biologia
                 {
                     //RECOMENDACION LIBRO DE BIOLOGIA
@@ -66,10 +66,10 @@ public class Encuesta {
         }//Terminan los textos realistas
         else if (opcion == 'F' || opcion == 'f')//Lo que pasa si elige un texto ficticio
         {
-            opcion = JOptionPane.showInputDialog("¿Usted prefiere las historias de terror(Escriba T), la ciencia ficcion(Escriba C)\n o las historias de fantasia(Escriba F)").charAt(0);
+            opcion = JOptionPane.showInputDialog("¿Usted prefiere las historias de terror(Escriba T), de ciencia ficción(Escriba C)\n o las historias de fantasía(Escriba F)").charAt(0);
             if (opcion == 'F' || opcion == 'f')//Lo que pasa si elige fantasia
             {
-                opcion = JOptionPane.showInputDialog("¿Prefiere las travesias de fantasia(Escriba T) o los cuentos de fantasia(Escriba C)?").charAt(0);
+                opcion = JOptionPane.showInputDialog("¿Prefiere las travesías de fantasía(Escriba T) o los cuentos de fantasía(Escriba C)?").charAt(0);
                 if (opcion == 'T' || opcion == 't')//Lo que pasa si elige travesias de fantasia
                 {
                     //RECOMENDACIONES DE LIBROS DE TRAVESIA FANTASTICA
@@ -88,7 +88,7 @@ public class Encuesta {
             }//Termina terror
             else if (opcion == 'C' || opcion == 'c')//Lo que pasa si elige ciencia ficcion
             {
-                opcion = JOptionPane.showInputDialog("¿Le gusta lo relacionado a la tecnologia (Escriba T), o si le gusta lo relacionado con viajes espaciales (Escriba E)\n o si le gustan los mundos post-apocalipticos (Escriba P)").charAt(0);
+                opcion = JOptionPane.showInputDialog("¿Le gusta lo relacionado a la tecnología (Escriba T), o si le gusta lo relacionado con viajes espaciales (Escriba E)\n o si le gustan los mundos post-apocalípticos (Escriba P)").charAt(0);
                 if (opcion == 'T' || opcion == 't')//Lo que pasa si eligio tecnologia
                 {
                     //RECOMENDACIONES DE LIBROS DE CIENCIA FICCION DE TECNOLOGIA
@@ -108,7 +108,7 @@ public class Encuesta {
         }
         else//Lo que pasa si no introduce el valor requerido
         {
-            JOptionPane.showMessageDialog(null,"Vuela a intentarlo introduciendo un valor valido");
+            JOptionPane.showMessageDialog(null,"Vuelva a intentarlo introduciendo un valor válido");
         }
     }
     
